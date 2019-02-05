6336d193995d8b6e527f280e097fdeb1f181da10
Sean Leclerc <47339189+SeanLeclerc@users.noreply.github.com>
2019-02-04 19:38:55 -0500

    Add files via upload

---
+++ Essai.java
@@ -0,0 +1,11 @@
+
+public class Essai {
+
+	public static void main(String[] args) {
+		Exemple e = new Exemple(2, 15);
+		System.out.println("X est " + e.getX());
+		System.out.println("Y est "  + e.getY());
+
+	}
+
+}
---
+++ Exemple.java
@@ -0,0 +1,16 @@
+
+public class Exemple {
+	private int x;
+	private int y;
+	public Exemple(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+}
+
